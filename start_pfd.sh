5aa080b8214b154d6d910391173023ef878a7c8b
yinghuali <yinghua.li@uni.lu>
2023-06-28 22:13:50 +0200

    update

---
+++ start_pfd.sh
@@ -0,0 +1,6 @@
+python vrank_pfd.py --save_path 'results/pfd/ucf101_C3D_18.json'  --path_x '/raid/yinghua/VRank/data/pkl_data/ucf101/ucf101_x.pkl' --path_y '/raid/yinghua/VRank/data/pkl_data/ucf101/ucf101_y.pkl' --path_val_pre './target_models/ucf101_C3D_18_val_pre.pkl' --path_test_pre './target_models/ucf101_C3D_18_test_pre.pkl' --path_x_embedding '/raid/yinghua/VRank/data/pkl_data/ucf101/ucf101_x_embedding.pkl'
+
+
+
+
+
