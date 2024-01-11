e1baa8623be50373cecae209b3c4e9ced9c62e14
yinghuali <yinghua.li@uni.lu>
2024-01-11 16:14:30 +0100

    update

--- get_frame_fearure.py
+++ get_frame_fearure.py
@@ -53,14 +53,33 @@ def get_squared_frame(x):
     return all_feature
 
 
-def main():
-    x = pickle.load(open(path_x, 'rb'))
+def get_pearson_frame(x):
+    all_feature = []
+    for video in x:
+        tmp_feature = []
+        n_frame = len(video)
+        for i in range(1, n_frame):
+            current_vec = video[i].reshape(-1)
+            previous_vec = video[i-1].reshape(-1)
+            diff = np.corrcoef(current_vec, previous_vec)[0, 1]
+            tmp_feature.append(diff)
+        all_feature.append(tmp_feature)
+    all_feature = np.array(all_feature)
+    return all_feature
 
-    print(x.shape)
-    feature = get_manhattan_frame(x)
-    print(feature.shape)
+
+def get_frame_diff(x):
+    feature_euclidean = get_euclidean_frame(x)
+    feature_manhattan = get_manhattan_frame(x)
+    feature_squared = get_squared_frame(x)
+    feature_pearson = get_pearson_frame(x)
+    frame_diff = np.hstack((feature_euclidean, feature_manhattan, feature_squared, feature_pearson))
+    return frame_diff
 
 
-if __name__ == '__main__':
-    main()
-
+# if __name__ == '__main__':
+#     x = pickle.load(open(path_x, 'rb'))
+#     print(x.shape)
+#     frame_diff = get_frame_diff(x)
+#     print(frame_diff.shape)
+#
