641206e7b7e9d92b27ffceec8cffaa5035393412
yinghuali <yinghua.li@uni.lu>
2024-01-12 21:59:02 +0100

    update

--- example/start_get_video_embedding.sh
+++ example/start_get_video_embedding.sh
@@ -26,4 +26,11 @@ python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/accident_nois
 python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/accident_noise/augmentation_zca_whitening_x.pkl' --save_video_embedding './pkl_data/accident_noise/augmentation_zca_whitening_x_embedding.pkl'
 
 
-
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd/ccd_x.pkl' --save_video_embedding './pkl_data/ccd/ccd_x_embedding.pkl'
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd_noise/augmentation_channel_shift_range_x.pkl' --save_video_embedding './pkl_data/ccd_noise/augmentation_channel_shift_range_x_embedding.pkl'
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd_noise/augmentation_featurewise_std_normalization_x.pkl' --save_video_embedding './pkl_data/ccd_noise/augmentation_featurewise_std_normalization_x_embedding.pkl'
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd_noise/augmentation_height_shift_x.pkl' --save_video_embedding './pkl_data/ccd_noise/augmentation_height_shift_x_embedding.pkl'
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd_noise/augmentation_horizontal_flip_x.pkl' --save_video_embedding './pkl_data/ccd_noise/augmentation_horizontal_flip_x_embedding.pkl'
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd_noise/augmentation_shear_range_x.pkl' --save_video_embedding './pkl_data/ccd_noise/augmentation_shear_range_x_embedding.pkl'
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd_noise/augmentation_width_shift_x.pkl' --save_video_embedding './pkl_data/ccd_noise/augmentation_width_shift_x_embedding.pkl'
+python get_video_embedding.py --cuda 'cuda:0' --path_x './pkl_data/ccd_noise/augmentation_zca_whitening_x.pkl' --save_video_embedding './pkl_data/ccd_noise/augmentation_zca_whitening_x_embedding.pkl'
